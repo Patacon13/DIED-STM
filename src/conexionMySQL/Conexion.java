3298649ec0988f2d1d9a8e0b62d60ccbdf3f2c3b
Irina Argañaras <irihuilen48@gmail.com>
2021-07-17 23:50:27 -0300

    BD MySQL y Test de conexion a BD

--- src/conexionMySQL/Conexion.java
+++ src/conexionMySQL/Conexion.java
@@ -1,27 +1,33 @@
-package conexionPostgreSQL;
+package conexionMySQL;
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class Conexion {
-	static final String URL = "jdbc:postgresql://localhost:puerto/baseDeDatos";
-	static final String USER = "nombreUsuario";
-	static final String PASS = "passwordUsuario";
+	 String URL = "jdbc:mysql://db4free.net:3306/sistdetransporte";
+	 String USER = "tpdied";
+	 String PASS = "tpdied1234";
 	
 	public Connection crearConexion() throws SQLException, ClassNotFoundException {
 	Connection conn = null;
 	try {
-	Class.forName("org.postgresql.Driver");
+	Class.forName("com.mysql.cj.jdbc.Driver");
 	conn = DriverManager.getConnection(URL, USER, PASS);
 	System.out.println("Conexión establecida");
 	}
 	catch (ClassNotFoundException e) {
 		e.printStackTrace(); //se captura cuando en el path no se encuentra la clase del driver
+		System.out.println("Error de ClassNotFoundException");
 		}
 	catch(SQLException e) {
 		e.printStackTrace(); //se captura cuando la ip, pass, user están mal o algún problema de la BD
+		System.out.println("Error de SQL");
 	}
 	return conn;
+	
 	}
+
 }
