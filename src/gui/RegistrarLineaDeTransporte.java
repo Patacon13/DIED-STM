2a50c68476f5aadbdb67dc7c109fa9e4931d3f10
vFontana0 <valefontana15@gmail.com>
2021-07-27 10:53:46 -0300

    Se agrego la funcionalidad de registrar una linea de transporte en la BD

--- src/gui/RegistrarLineaDeTransporte.java
+++ src/gui/RegistrarLineaDeTransporte.java
@@ -2,17 +2,36 @@ package gui;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+
+import java.awt.Color;
 import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import estacion.Estacion;
+import estacion.EstadoEstacion;
+import lineaDeTransporte.AdministradorDeLineasDeTransporte;
+import lineaDeTransporte.ColorLineaDeTransporte;
+import lineaDeTransporte.EstadoLinea;
+import lineaDeTransporte.LineaDeTransporte;
+
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JButton;
+import javax.swing.JColorChooser;
+
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 
 public class RegistrarLineaDeTransporte extends JPanel {
 	private JTextField linNombre;
-	private JTextField linColor;
 
 	/**
 	 * Create the panel.
@@ -20,9 +39,9 @@ public class RegistrarLineaDeTransporte extends JPanel {
 	public RegistrarLineaDeTransporte() {
 
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{33, 136, 62, 189, 0};
+		gridBagLayout.columnWidths = new int[]{33, 136, 62, 189, 34, 0};
 		gridBagLayout.rowHeights = new int[]{19, 35, 20, 20, 20, 22, 35, 23, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
@@ -30,7 +49,7 @@ public class RegistrarLineaDeTransporte extends JPanel {
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.fill = GridBagConstraints.VERTICAL;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel.gridwidth = 3;
 		gbc_lblNewLabel.gridx = 1;
 		gbc_lblNewLabel.gridy = 0;
@@ -48,7 +67,7 @@ public class RegistrarLineaDeTransporte extends JPanel {
 		GridBagConstraints gbc_estNombre = new GridBagConstraints();
 		gbc_estNombre.anchor = GridBagConstraints.NORTH;
 		gbc_estNombre.fill = GridBagConstraints.HORIZONTAL;
-		gbc_estNombre.insets = new Insets(0, 0, 5, 0);
+		gbc_estNombre.insets = new Insets(0, 0, 5, 5);
 		gbc_estNombre.gridx = 3;
 		gbc_estNombre.gridy = 2;
 		add(linNombre, gbc_estNombre);
@@ -62,15 +81,26 @@ public class RegistrarLineaDeTransporte extends JPanel {
 		gbc_lblNewLabel_1_1.gridy = 3;
 		add(lblNewLabel_1_1, gbc_lblNewLabel_1_1);
 		
-		linColor = new JTextField();
-		linColor.setColumns(10);
-		GridBagConstraints gbc_estApertura = new GridBagConstraints();
-		gbc_estApertura.anchor = GridBagConstraints.NORTH;
-		gbc_estApertura.fill = GridBagConstraints.HORIZONTAL;
-		gbc_estApertura.insets = new Insets(0, 0, 5, 0);
-		gbc_estApertura.gridx = 3;
-		gbc_estApertura.gridy = 3;
-		add(linColor, gbc_estApertura);
+		JComboBox<ColorLineaDeTransporte> linColor = new JComboBox<ColorLineaDeTransporte>();
+		GridBagConstraints gbc_linColor = new GridBagConstraints();
+		gbc_linColor.insets = new Insets(0, 0, 5, 5);
+		gbc_linColor.fill = GridBagConstraints.HORIZONTAL;
+		gbc_linColor.gridx = 3;
+		gbc_linColor.gridy = 3;
+		linColor.addItem(ColorLineaDeTransporte.AMARILLO);
+		linColor.addItem(ColorLineaDeTransporte.AZUL);
+		linColor.addItem(ColorLineaDeTransporte.BLANCO);
+		linColor.addItem(ColorLineaDeTransporte.CYAN);
+		linColor.addItem(ColorLineaDeTransporte.GRIS);
+		linColor.addItem(ColorLineaDeTransporte.GRIS_CLARO);
+		linColor.addItem(ColorLineaDeTransporte.GRIS_CLARO);
+		linColor.addItem(ColorLineaDeTransporte.MAGENTA);
+		linColor.addItem(ColorLineaDeTransporte.NARANJA);
+		linColor.addItem(ColorLineaDeTransporte.NEGRO);
+		linColor.addItem(ColorLineaDeTransporte.ROJO);
+		linColor.addItem(ColorLineaDeTransporte.ROSADO);
+		linColor.addItem(ColorLineaDeTransporte.VERDE);
+		add(linColor, gbc_linColor);
 		
 		
 		JLabel lblNewLabel_1_2 = new JLabel("Estado:  ");
@@ -81,29 +111,63 @@ public class RegistrarLineaDeTransporte extends JPanel {
 		gbc_lblNewLabel_1_2.gridy = 4;
 		add(lblNewLabel_1_2, gbc_lblNewLabel_1_2);
 		
-		JComboBox<String> linEstado = new JComboBox<String>();
+		JComboBox<EstadoLinea> linEstado = new JComboBox<EstadoLinea>();
 		GridBagConstraints gbc_estEstado = new GridBagConstraints();
 		gbc_estEstado.fill = GridBagConstraints.BOTH;
-		gbc_estEstado.insets = new Insets(0, 0, 5, 0);
+		gbc_estEstado.insets = new Insets(0, 0, 5, 5);
 		gbc_estEstado.gridx = 3;
 		gbc_estEstado.gridy = 4;
 		add(linEstado, gbc_estEstado);
-		linEstado.addItem("Activa");
-		linEstado.addItem("No Activa");
+		linEstado.addItem(EstadoLinea.ACTIVO);
+		linEstado.addItem(EstadoLinea.INACTIVO);
 		
-		JButton btnNewButton = new JButton("Agregar Linea de Transporte");
+		JLabel labelErrores = new JLabel("");
+		labelErrores.setForeground(Color.RED);
+		labelErrores.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		GridBagConstraints gbc_labelErrores = new GridBagConstraints();
+		gbc_labelErrores.gridwidth = 3;
+		gbc_labelErrores.insets = new Insets(0, 0, 5, 5);
+		gbc_labelErrores.gridx = 1;
+		gbc_labelErrores.gridy = 6;
+		add(labelErrores, gbc_labelErrores);
+		
+
+
+
+		JButton boton = new JButton("Agregar Linea de Transporte");
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
 		gbc_btnNewButton.anchor = GridBagConstraints.NORTH;
 		gbc_btnNewButton.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnNewButton.gridwidth = 3;
 		gbc_btnNewButton.gridx = 1;
-		gbc_btnNewButton.gridy = 6;
-		add(btnNewButton, gbc_btnNewButton);
+		gbc_btnNewButton.gridy = 7;
+		add(boton, gbc_btnNewButton);
 		
-		btnNewButton.addActionListener(e->{
-				//Accion cuando se presione el boton
+		boton.addActionListener(new ActionListener() {
+				//linColor deberia ser un comboBox
+			public void actionPerformed(ActionEvent e) {
+				AdministradorDeLineasDeTransporte admin = new AdministradorDeLineasDeTransporte();
+				labelErrores.setForeground(Color.RED);
+				if(linNombre.getText().length() == 0)  {
+					labelErrores.setText("Has dejado algún campo sin completar, revisalo.");
+				} else {
+					EstadoLinea estado = linEstado.getItemAt(linEstado.getSelectedIndex());
+					ColorLineaDeTransporte color = linColor.getItemAt(linEstado.getSelectedIndex());
+					LineaDeTransporte nueva = new LineaDeTransporte(null, linNombre.getText().toString(), color, estado);
+					try {
+						admin.addlinea(nueva);
+						labelErrores.setForeground(Color.GREEN);
+						labelErrores.setText("Se registro la estacion correctamente");
+						linNombre.setText("");
+					} catch (ClassNotFoundException | SQLException e1) {
+						labelErrores.setText(e1.getMessage());
+					}
+				}
+			}
 		});
 
 	}
+	
+	
 }
