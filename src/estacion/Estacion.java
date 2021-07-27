b5d3a78bdb379fdd82a418c29911903911cfb5f2
vFontana0 <valefontana15@gmail.com>
2021-07-27 10:49:13 -0300

    Se agregaron setters y getters y se agrego atributo ID

--- src/estacion/Estacion.java
+++ src/estacion/Estacion.java
@@ -1,19 +1,92 @@
 package estacion;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalTime;
+import java.util.Date;
+
+import conexionMySQL.Conexion;
+import tareaDeMantenimiento.TareaDeMantenimiento;
 
 public class Estacion {
+	private Conexion con = new Conexion();
 	protected Integer id;
 	protected String nombre;
 	protected LocalTime horarioApertura;
 	protected LocalTime horarioCierre;
 	protected EstadoEstacion estado;
 	
-	protected Estacion(Integer id, String nombre, LocalTime horarioApertura, LocalTime horarioCierre, EstadoEstacion estado) {
+	public Estacion(Integer id, String nombre, LocalTime horarioApertura, LocalTime horarioCierre, EstadoEstacion estado) {
 		this.id = id;
 		this.nombre = nombre;
 		this.horarioApertura = horarioApertura;
 		this.horarioCierre = horarioCierre;
 		this.estado = estado;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public LocalTime getHorarioApertura() {
+		return horarioApertura;
+	}
+
+	public LocalTime getHorarioCierre() {
+		return horarioCierre;
+	}
+
+	public EstadoEstacion getEstado() {
+		return estado;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setHorarioApertura(LocalTime horarioApertura) {
+		this.horarioApertura = horarioApertura;
+	}
+
+	public void setHorarioCierre(LocalTime horarioCierre) {
+		this.horarioCierre = horarioCierre;
+	}
+
+	public void setEstado(EstadoEstacion estado) {
+		this.estado = estado;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.nombre;
+	}
+
+	public TareaDeMantenimiento obtenerUltimoMantenimiento() throws SQLException, ClassNotFoundException {
+		Connection conn = con.crearConexion();
+		PreparedStatement pstm = conn.prepareStatement("SELECT * FROM mantenimiento WHERE estacion=?");
+		pstm.setInt(1, this.getId());
+		ResultSet rs = pstm.executeQuery();
+		TareaDeMantenimiento ret = new TareaDeMantenimiento();
+	    while(rs.next()) {
+	    	ret.setId(rs.getInt(1));
+	    	ret.setEstacion(this);
+	    	ret.setFechaInicioTarea(rs.getDate(3).toLocalDate());
+	    }
+		pstm.close();
+		conn.close();
+	return ret;
+	}
+	
+	
 }
