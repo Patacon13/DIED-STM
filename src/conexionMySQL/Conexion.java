f7c339d966bb2615aa6fea05dfb403f050078c82
vFontana0 <valefontana15@gmail.com>
2021-07-31 11:15:53 -0300

    Modificada conexion de BD para uso local

--- src/conexionMySQL/Conexion.java
+++ src/conexionMySQL/Conexion.java
@@ -7,27 +7,16 @@ import java.sql.SQLException;
 
 
 public class Conexion {
-	 String URL = "jdbc:mysql://db4free.net:3306/sistdetransporte";
-	 String USER = "tpdied";
-	 String PASS = "tpdied1234";
+	 String URL = "jdbc:mysql://localhost:3306/tpdied";
+	 String USER = "root";
+	 String PASS = "";
 	
 	public Connection crearConexion() throws SQLException, ClassNotFoundException {
 	Connection conn = null;
-	try {
 	Class.forName("com.mysql.cj.jdbc.Driver");
 	conn = DriverManager.getConnection(URL, USER, PASS);
-	System.out.println("Conexión establecida");
-	}
-	catch (ClassNotFoundException e) {
-		e.printStackTrace(); //se captura cuando en el path no se encuentra la clase del driver
-		System.out.println("Error de ClassNotFoundException");
-		}
-	catch(SQLException e) {
-		e.printStackTrace(); //se captura cuando la ip, pass, user están mal o algún problema de la BD
-		System.out.println("Error de SQL");
-	}
+	//System.out.println("Conexión establecida");
 	return conn;
-	
 	}
 
 }
