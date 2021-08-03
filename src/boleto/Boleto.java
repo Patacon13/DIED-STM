66c3ca23fb918c942416cc8f41a6929b05ac36c3
vFontana0 <valefontana15@gmail.com>
2021-08-03 00:13:08 -0300

    Cambios en atributos y constructor

--- src/boleto/Boleto.java
+++ src/boleto/Boleto.java
@@ -7,33 +7,102 @@ import estacion.Estacion;
 import sources.*;
 
 public class Boleto {
-	protected Integer numero;
-	protected String correoElectronico;
-	protected String nombre;
-	protected LocalDate fechaDeVenta;
-	protected String nombreEstacionOrigen;
-	protected String nombreEstacionDestino;
-	protected List<Estacion> caminoASeguir;
-	protected Boleto(Integer numero, String correoElectronico, String nombre, LocalDate fechaDeVenta,
-			String nombreEstacionOrigen, String nombreEstacionDestino, List<Estacion> caminoASeguir) {
+	private Integer numero;
+	private String correoElectronico;
+	private String nombre;
+	private LocalDate fechaDeVenta;
+	private Estacion origen;
+	private Estacion destino;
+	private Double costo;
+	private List<Estacion> caminoASeguir;
+
+
+	public Boleto(Integer numero, String correoElectronico, String nombre, LocalDate fechaDeVenta,
+			Estacion origen, Estacion destino, Double costo, List<Estacion> caminoASeguir) {
 		this.numero = numero;
 		this.correoElectronico = correoElectronico;
 		this.nombre = nombre;
 		this.fechaDeVenta = fechaDeVenta;
-		this.nombreEstacionOrigen = nombreEstacionOrigen;
-		this.nombreEstacionDestino = nombreEstacionDestino;
+		this.origen = origen;
+		this.destino = destino;
 		this.caminoASeguir = caminoASeguir;
+		this.costo = costo;
+	}
+
+
+	public Integer getNumero() {
+		return numero;
 	}
 	
-	protected Boleto(Integer numero, String correoElectronico, String nombre, LocalDate fechaDeVenta,
-			String nombreEstacionOrigen, String nombreEstacionDestino) {
-		this.numero = numero;
-		this.correoElectronico = correoElectronico;
-		this.nombre = nombre;
-		this.fechaDeVenta = fechaDeVenta;
-		this.nombreEstacionOrigen = nombreEstacionOrigen;
-		this.nombreEstacionDestino = nombreEstacionDestino;
+	public Double getCosto() {
+		return costo;
 	}
+
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+
+
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public LocalDate getFechaDeVenta() {
+		return fechaDeVenta;
+	}
+
+
+	public void setFechaDeVenta(LocalDate fechaDeVenta) {
+		this.fechaDeVenta = fechaDeVenta;
+	}
+
+
+	public Estacion getOrigen() {
+		return origen;
+	}
+
+
+	public void setOrigen(Estacion origen) {
+		this.origen = origen;
+	}
+
+
+	public Estacion getDestino() {
+		return destino;
+	}
+
+
+	public void setDestino(Estacion destino) {
+		this.destino = destino;
+	}
+
+
+	public List<Estacion> getCaminoASeguir() {
+		return caminoASeguir;
+	}
+
+
+	public void setCaminoASeguir(List<Estacion> caminoASeguir) {
+		this.caminoASeguir = caminoASeguir;
+	}
+
 	
 	
 }
